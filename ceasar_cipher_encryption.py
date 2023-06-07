5717e165cd9f87d78362701b46f85bf30e5ca313
MideBillz <olumideakinsolabillz@gmail.com>
2023-06-07 08:21:45 -0400

    added art.py to project for better experience and re-factored code to make it possible to either continue or end the program after the user is done

--- ceasar_cipher_encryption.py
+++ ceasar_cipher_encryption.py
@@ -1,63 +1,52 @@
 #!/usr/bin/env python
 
+# Importing logo from art.py
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-# Combined the encrypt() and decrypt() functions below into a single function called caesar().
-# Also added a new variable called
+# Creating a single function called ceasar() able to encode/decode.
 
 def caesar(input_text, shift_amount, cipher_direction):
     final_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in input_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        final_text += alphabet[new_position]
+    # Note: Changed "letter" variable to "character"
+    for character in input_text:
+        # Checking if Characters entered by user is in the list of characters provided
+        if character in alphabet:
+            position = alphabet.index(character)
+            new_position = position + shift_amount
+            final_text += alphabet[new_position]
+        else:
+            # Encoding/Decoding only the letters and adding other characters entered by the user as they are.
+            final_text += character
 
-    # using an f string to allow "direction" variable to be used dynamically in the final print statement
-    print(f"Here's the {direction}d result: {final_text}")
-
-
-caesar(input_text=text, shift_amount=shift, cipher_direction=direction)
-
-
-# Encrypt function to encode text based on user direction input. This function shifts each letter of the 'text'
-# *forward* in the alphabet by the shift amount and prints the encrypted text. e.g. input_text = "hello",
-# shift = 5, encoded_text = #"mjqqt" . Function prints out: "The encoded text is mjqqt"
-
-# def encrpyt(input_text, shift_amount):
-#     encoded_text = ""
-#     for letter in input_text:
-#         letter_position = alphabet.index(letter)
-#         new_letter_position = letter_position + shift_amount
-#         new_letter = alphabet[new_letter_position]
-#         encoded_text += new_letter
-#     print(f"The encoded text is {encoded_text}")
+    # using an f string to allow "cipher_direction" variable to be used dynamically in the final print statement
+    print(f"Here's the {cipher_direction}d result: {final_text}")
 
 
-# Decrypt function to decode text based on user direction input. This function shifts each letter of the 'text'
-# *backwards* in the alphabet by the shift amount and prints the decrypted text. e.g. encoded_text = "mjqqt",
-# shift = 5, input_text = "hello" . Function prints out: "The decoded text is hello"
+# Printing the cipher logo imported for art.py to show at the start of the program
+print(logo)
 
-# def decrypt(input_text, shift_amount):
-#     decoded_text = ""
-#     for letter in input_text:
-#         letter_position = alphabet.index(letter)
-#         new_letter_position = letter_position - shift_amount
-#         new_letter = alphabet[new_letter_position]
-#         decoded_text += new_letter
-#     print(f"The decoded text is {decoded_text}")
-#
-#
-# if direction == "encode":
-#     encrpyt(input_text=text, shift_amount=shift)
-# elif direction:
-#     decrypt(input_text=text, shift_amount=shift)
+should_Continue = True
+while should_Continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    # Catching a bug here incase user enters a shift that is greater than the number of letters in the alphabet.
+    # For example running the program and entering a shift number of 45, the program continues to work even if the user
+    # enters a shift number greater than 26.
+    shift = shift % 26
+    caesar(input_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_Continue = False
+        print("Goodbye")
+
